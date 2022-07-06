e66339f30d45b60de84f9dfe36b2deea207140d1
Robbie Woolf <robbiemwoolf@gmail.com>
2022-07-06 08:49:17 -0700

    using helper function api fetching is working and decks delete correctly

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
 import Header from "./Header";
 import NotFound from "./NotFound";
+import Home from "./home/Home";
 
 function Layout() {
   return (
     <>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
-        <NotFound />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+        
       </div>
     </>
   );
